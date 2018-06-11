4ae1062afc0f7f11edd90e23a7be20eff6a3d4ea
Seth Wallin <seth.wallin@gmail.com>
2018-06-11 08:26:15 -0400

    makin' it look nice..

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ function carousel() {
     var i;
     var x = document.getElementsByClassName("slides");
     for (i = 0; i < x.length; i++) {
-      x[i].style.opacity = "0"; 
+      x[i].style.display = "none"; 
     }
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1} 
-    x[slideIndex-1].style.opacity = "1"; 
+    x[slideIndex-1].style.display = "inline"; 
     setTimeout(carousel, 3000); // Change image every 2 seconds
 }
